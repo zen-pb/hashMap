7e40deaf9cb9acd94b9cb464157a448dd70164bd
Edizen Buguina <111935236+zen-pb@users.noreply.github.com>
2025-07-24 22:04:09 +0800

    Fix same instance of LinkedList per bucket

--- HashMap.js
+++ HashMap.js
@@ -2,7 +2,7 @@ import LinkedList from "./LinkedList.js";
 
 class HashMap {
     constructor(load = 0.8, capacity = 17){
-        this.buckets = Array(capacity).fill(new LinkedList())
+        this.buckets = Array.from({ length: capacity }, () => new LinkedList());
         this.load_factor = load;
         this.capacity = capacity;
     }
