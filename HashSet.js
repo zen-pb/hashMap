e673551dba8827398f474482a6bd023ecabcd804
Edizen Buguina <111935236+zen-pb@users.noreply.github.com>
2025-07-25 11:36:19 +0800

    Create a HashSet.js for storing keys only

---
+++ HashSet.js
@@ -0,0 +1,123 @@
+import LinkedList from "./LinkedList.js";
+
+class HashSet {
+    constructor(load = 0.8, capacity = 17){
+        this.buckets = Array.from({ length: capacity }, () => new LinkedList());
+        this.load_factor = load;
+        this.capacity = capacity;
+        this.numberOfBuckets = 0;
+    }
+
+    hash(key) {
+        let hashCode = 0;
+            
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+        }
+
+        return hashCode;
+    } 
+
+    set(key){
+        if(this.has(key)) return;
+
+        const index = this.hash(key);
+        const bucket = this.buckets[index];
+
+        bucket.append({key});
+        if(bucket.size() === 1){
+            this.numberOfBuckets += 1;
+        }
+
+        if(this.numberOfBuckets / this.capacity >= this.load_factor){
+            this.resize();
+        }
+
+        return;
+    }
+
+    has(key){
+        const index = this.hash(key);
+        const bucket = this.buckets[index];
+
+        let current = bucket.head;
+        while (current) {
+            if (current.value.key === key){
+                return true;
+            }
+            current = current.nextNode;
+        }
+
+        return false;
+    }
+
+    remove(key){
+        const index = this.hash(key);
+        const bucket = this.buckets[index];
+
+        let current = bucket.head;
+        let counter = 0;
+        while (current) {
+            if (current.value.key === key){
+                bucket.removeAt(counter);
+                if(bucket.size() === 0){
+                    this.numberOfBuckets -= 1;
+                }
+                return true;
+            }
+            current = current.nextNode;
+            counter += 1;
+        }
+
+        return false;
+    }
+
+    length(){
+        let numberOfStoredKeys = 0;
+        for(let i = 0; i < this.capacity; i++){
+            let bucket = this.buckets[i];
+
+            numberOfStoredKeys += bucket.size();
+        }
+
+        return numberOfStoredKeys;
+    }
+
+    clear(){
+        this.buckets = Array.from({ length: this.capacity }, () => new LinkedList());
+    }
+
+    keys(){
+        let keysArray = [];
+        for(let i = 0; i < this.capacity; i++){
+            let bucket = this.buckets[i];
+
+            let current = bucket.head;
+            while(current) {
+                keysArray.push(current.value.key);
+                current = current.nextNode;
+            }
+        }
+
+        return keysArray;
+    }
+
+    resize() {
+        const oldBuckets = this.buckets;
+        this.capacity = this.capacity * 2;
+        this.buckets = Array.from({ length: this.capacity }, () => new LinkedList());
+        this.size = 0; 
+
+        for (const bucket of oldBuckets) {
+            let current = bucket.head;
+            while (current) {
+                const { key } = current.value;
+                this.set(key); 
+                current = current.nextNode;
+            }
+        }
+    }
+};
+
+export default HashSet;
